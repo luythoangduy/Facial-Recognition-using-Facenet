867586cfe5960381e6e0db928a5cb2184b9b6ee2
luythoangduy <161418556+luythoangduy@users.noreply.github.com>
2024-08-02 22:19:28 +0700

    Update inception_blocks_v2.py

--- utils/inception_blocks_v2.py
+++ utils/inception_blocks_v2.py
@@ -4,7 +4,7 @@ from keras import backend as K
 from keras.layers import Conv2D, ZeroPadding2D, Activation, Input, concatenate
 from keras.models import Model
 from keras.layers import BatchNormalization
-from keras.layers.pooling import MaxPooling2D, AveragePooling2D
+from keras.layers import MaxPooling2D, AveragePooling2D
 from utils import fr_utils
 from keras.layers.core import Lambda, Flatten, Dense
 
